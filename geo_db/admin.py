8451bc6955b78e88750fc50b17a99ed9fdef5e63
Arxonait <nikk23716@gmail.com>
2024-05-05 17:13:16 +0300

    добавлено админка столиц

--- geo_db/admin.py
+++ geo_db/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from geo_db.models import Country, City, Photo
+from geo_db.models import Country, City, Photo, Capital
 
 
 class PhotoInline(admin.TabularInline):
@@ -21,6 +21,13 @@ class CityAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
+class CapitalAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+
+
 admin.site.register(Country, CountryAdmin)
 admin.site.register(City, CityAdmin)
+admin.site.register(Capital, CapitalAdmin)
 
